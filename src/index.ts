c238bcc9fe3c3436a3aa558b09476834bbbfba48
augustofrade <aquaaticnaotrava@gmail.com>
2024-02-14 20:24:59 -0300

    sql branch

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { diarySetup } from './cli/diarySetup';
 import { mainMenu } from './cli/mainMenu';
 import { ConfigManager } from './util/ConfigManager';
 import { initCLI } from './util/initCLI';
+import { DailyEntryService } from './service/DailyEntryService';
+import { IDailyEntry } from './types/IDailyEntry';
 
 const cm = ConfigManager.instance();
 
@@ -13,9 +15,22 @@ if(!cm.hasConfigFile()) {
 } else {
     initCLI()
     .then(() => {
-        mainMenu();
+        // mainMenu();
+        const e: IDailyEntry = {
+            dateID: "2024-02-14",
+            title: "teste sql",
+            description: "descricao",
+            mood: 2,
+            wordCount: 1,
+            creationDate: new Date(),
+            modificationDate: new Date(),
+            categories: []
+        }
+        const res = DailyEntryService.instance().addEntry(e);
+        console.log(res);
     })
-    .catch(() => {
+    .catch((e) => {
+        console.log(e)
         console.log(chalk.red("Failed to fetch configuration data and set storage method"))
     });
 }
